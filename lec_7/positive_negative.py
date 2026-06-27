3997e099b345bcfe7cf85818ca7fcaeb7d73b01f
sanjusolanki20004-eng <sanjusolanki20004@gmail.com>
2026-06-27 16:51:17 +0530

    Add files via upload

---
+++ lec_7/positive_negative.py
@@ -0,0 +1,11 @@
+num = int(input("Enter a number :"))
+
+if num > 0 :
+    print(f"{num} is positive number")
+    
+elif num < 0 :
+    print(f"{num} is negitive number")  
+    
+else :
+    print("number is invalid")    
+    
