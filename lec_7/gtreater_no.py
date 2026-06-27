8568baf5c9624a9a933087cbf72c49fa0792da18
sanjusolanki20004-eng <sanjusolanki20004@gmail.com>
2026-06-27 16:44:15 +0530

    Add files via upload

---
+++ lec_7/gtreater_no.py
@@ -0,0 +1,13 @@
+n1 =int(input("Enter a  first number :"))
+n2 =int(input("Enter a  second number :"))
+
+if n1 > n2 :
+    print(f"{n1} is greater than {n2}")
+    
+elif  n2 > n1 :
+    print(f"{n2} is greater than {n1}")
+
+else :
+    print ("both are equal")
+    
+    
