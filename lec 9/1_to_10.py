b8e4456a1dfdd3b933a8d991cb8d291f4a43c417
sanjusolanki20004-eng <sanjusolanki20004@gmail.com>
2026-06-28 15:23:53 +0530

    Add files via upload

---
+++ lec 9/1_to_10.py
@@ -0,0 +1,9 @@
+# program to print 1 to 10 no
+num = 1
+
+while num <= 10 :
+    print(f"{num}")
+    num = num + 1
+    
+    
+    
---
+++ lec 9/count_digit.py
@@ -0,0 +1,10 @@
+num = int(input("enter your number :"))
+count = 0
+while num > 0 :
+    num //= 10
+    count += 1
+print(f"the number of digit is given {count}")  
+  
+    
+    
+    
