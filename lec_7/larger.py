48592266cdffca04ccc7f5c4a99f2ef34ed86330
sanjusolanki20004-eng <sanjusolanki20004@gmail.com>
2026-06-28 10:04:14 +0530

    Add files via upload

---
+++ lec_7/larger.py
@@ -0,0 +1,17 @@
+num1 = int(input("Enter a first number :"))
+num2 = int(input("Enter a second number :"))
+num3 = int(input("Enter a third number :"))
+
+if num1 > num2 and num1 > num3 :
+ print(f"{num1} is greater the largest number")
+
+elif num2 > num1  and num2 > num3 :
+  print(f"{num2} is greater the lagest number")
+  
+else :
+  
+   print(f"{num3} is greater the lagest number")
+  
+  
+  
+  
