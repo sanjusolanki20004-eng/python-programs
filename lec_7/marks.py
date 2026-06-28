6e375f354fd33ec245609ba2c149c9caf078f1e0
sanjusolanki20004-eng <sanjusolanki20004@gmail.com>
2026-06-28 10:37:09 +0530

    Add files via upload

---
+++ lec_7/marks.py
@@ -0,0 +1,21 @@
+m1 = float(input("Enter your marks :"))
+m2 = float(input("Enter your marks :"))
+m3 = float(input("Enter your marks :"))
+m4 = float(input("Enter your marks :"))
+m5 = float(input("Enter your marks :"))
+
+total_marks = m1+m2+m3+m4+m5
+percentage = ((total_marks)/500)*100
+print(f"the total marks are obtained by student {total_marks}")
+print (f"total percentage are obtained by student {percentage}")
+
+if percentage >= 40 :
+ print ("pass")
+ 
+else :
+ print ("fail")
+ 
+ 
+ 
+ 
+    
---
+++ lec_7/num_ in_word.py
@@ -0,0 +1,29 @@
+num = int(input("Enter your number :"))
+
+if num == 1:
+    print ("one")
+    
+elif num == 2 :
+    print("two") 
+elif num == 3 :
+    print("three") 
+elif num == 4 :
+    print("four")    
+elif num == 5 :
+    print("five")      
+elif num == 6 :
+    print("six")   
+elif num == 7 :
+    print("seven")   
+elif num == 8 :
+    print("eight")  
+elif num == 9 :
+    print("nine")     
+elif num == 10 :
+    print("ten") 
+else :
+    print ("invalid no")  
+          
+             
+        
+           
